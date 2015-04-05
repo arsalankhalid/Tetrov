a787f5a91d42af6637d2568fc77510c394f939ef
Justin Flowers <jbflowers@myseneca.ca>
2015-04-05 12:40:31 -0400

    Changed Block name to TVBlock

---
+++ src/simpleslickgame/TVBlock.java
@@ -0,0 +1,47 @@
+package simpleslickgame;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.geom.Rectangle;
+
+public class TVBlock extends Rectangle{
+
+	private static final long serialVersionUID = 1L;
+
+	Graphics graphics;
+	GameContainer gc;
+	
+	public TVBlock(GameContainer gc, Grid grid) {
+		super(0, 0, 50, 50);
+		
+		// Get height and width of canvas and determine block size
+		int height = gc.getHeight();
+		int width = gc.getWidth();
+		
+		// 10 cells across, 22 cells high
+		height = height/22;
+		width = width/10;
+		
+		// Set height, width, graphics, and gamecontainer
+		setHeight(height);
+		setWidth(width);
+		
+		this.gc = gc;
+		this.graphics= gc.getGraphics();
+		
+	}
+	
+	public void drawBlock(){
+		g.draw(this);
+	}
+	
+	public boolean checkCollision(TVBlock[] blocks){
+		for(int i = 0; i < blocks.length; i++){
+			if (blocks[i].intersects(this)){
+				return true;
+			}
+		}
+		
+		return false;
+	}
+}
