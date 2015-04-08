1904e82bcea4108d063825eb20f9e232d07497ac
Arsalan Khalid <arsalankhalid@outlook.com>
2015-04-07 21:07:43 -0400

    Created definition for TVShapeFactory, and created method getRandomShape which will use RNG to retrun a random shape

---
+++ src/simpleslickgame/TVShapeFactory.java
@@ -0,0 +1,13 @@
+package simpleslickgame;
+
+import org.newdawn.slick.Color;
+import org.newdawn.slick.GameContainer;
+
+public class TVShapeFactory {
+	
+	//most likely TVShape, possibly Shape from slick lib 
+	public void getRandomShape() {
+		
+		//return type TVShape;
+	}
+}
