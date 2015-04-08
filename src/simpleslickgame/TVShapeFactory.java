16bcf85991737b3fc2cfc93c1424c4c51a52f93d
Arsalan Khalid <arsalankhalid@outlook.com>
2015-04-07 21:29:16 -0400

    Added definitions for creating objects in TVShapeFactory, also in process of creating more shapes

--- src/simpleslickgame/TVShapeFactory.java
+++ src/simpleslickgame/TVShapeFactory.java
@@ -4,6 +4,23 @@ import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 
 public class TVShapeFactory {
+
+	
+	private void makeTVLShape() {
+		
+	}
+	
+	private void makeTVIShape() {
+		
+	}
+	
+	private void makeTVZShape() {
+		
+	}
+	
+	private void makeTVBoxShape() {
+		
+	}
 	
 	//most likely TVShape, possibly Shape from slick lib 
 	public void getRandomShape() {
