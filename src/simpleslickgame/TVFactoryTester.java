9497dd3f2b2e6862504ecc98b7ee73097bb7efc7
Arsalan Khalid <arsalankhalid@outlook.com>
2015-04-08 23:27:54 -0400

    added constructor to TVShapeFactory to receive gamecontainer, creates shapes based on received gc

--- src/simpleslickgame/TVFactoryTester.java
+++ src/simpleslickgame/TVFactoryTester.java
@@ -1,5 +1,11 @@
 package simpleslickgame;
 
 public class TVFactoryTester {
-
+	public static void main(String[] args) {
+		TVShapeFactory shapeFactory = new TVShapeFactory();
+		
+		//Create set of Shapes
+		TVShape shape1 = shapeFactory.createShape("TVIShape");
+		TVShape shape2 = shapeFactory.createShape("TVLShape");
+	}
 }
