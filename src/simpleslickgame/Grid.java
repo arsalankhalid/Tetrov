e00cb9d16a9b0a4701fade0877bdc52bb1843886
Kabeer Rashid <kabeer.rashid25@gmail.com>
2015-04-08 22:06:04 -0400

    Updated

--- src/simpleslickgame/Grid.java
+++ src/simpleslickgame/Grid.java
@@ -3,19 +3,29 @@ package simpleslickgame;
 
 public class Grid{
 	
-	private TVBlock[][] gameboard;
+	TVBlock[][] gameboard;
+	int x;
+	int y;
+	int row;
+	int col;
 	
-	public void addShapetoTop(){}
+	public void addShapetoTop(){
+		
+	}
 	
-	public void updateCurrentShape(TVShape tvshape){}
+	public void updateCurrentShape(TVShape tvshape){
+		
+	}
 	
-	public void removeBlock(){}
+	public void removeBlock(){
+		
+	}
 	
 	public void moveRowDown(){}
 	
 	public void moveBlockDown(TVBlock tvblock){}
 	
-	
+	public void isCollided (TVShape tvshape){}
 	
 	
 
