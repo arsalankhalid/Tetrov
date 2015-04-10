26f0167f4559c61340a23cd3a32a0421794a53b6
Kabeer Rashid <kabeer.rashid25@gmail.com>
2015-04-09 22:26:16 -0400

    drawblocks implemented

--- src/simpleslickgame/TVGrid.java
+++ src/simpleslickgame/TVGrid.java
@@ -153,6 +153,16 @@ public class TVGrid{
 	}
 	
 	public void drawBlocks(){
+		for(int i =0; i < 22; i ++)
+		{
+			for(int j = 0; j < 10; j++)
+			{
+				if(gameboard[i][j] != null)
+				{
+					gameboard[i][j].drawBlock();
+				}
+			}
+		}
 		
 	}
 	
