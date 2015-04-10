50ffa521fe3228c087cdf40f56643f54cbada9c0
Justin Flowers <jbflowers@myseneca.ca>
2015-04-09 21:01:46 -0400

    Bugfixes for TVShape

--- src/simpleslickgame/TVShape.java
+++ src/simpleslickgame/TVShape.java
@@ -202,7 +202,8 @@ public abstract class TVShape extends Observable{
 	public void moveUp(){
 		for(int i=0;i<4;i++){
 			for(int c = 0; c < 4; c++){
-				blocks[i][c].moveUp();
+				if(blocks[i][c] != null)
+					blocks[i][c].moveUp();
 			}
 		}
 	}
